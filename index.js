73fc881bff7fe05bbf8ae36552224e06e02be809
Carmen Morata <cmoratagarcia@gmail.com>
2025-07-22 10:36:35 -0700

    Create calculator object and pass all the tests

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ function reverseString(string) {
   return string.split("").reverse().join("");
 }
 
+const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+};
+
 module.exports = {
   capitalize,
   reverseString,
+  calculator,
 };
