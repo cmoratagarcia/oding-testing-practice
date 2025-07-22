e35876a23f52f4297967370f589b9828392f530d
Carmen Morata <cmoratagarcia@gmail.com>
2025-07-22 10:19:56 -0700

    Write capitalize function

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const { capitalize } = require("./index.js");
 //A capitalize function that takes a string and returns it with the first character capitalized.
 test("capitalizes first character", () => {
   expect(capitalize("abcdef")).toBe("Abcdef");
