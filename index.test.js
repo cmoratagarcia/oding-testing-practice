3047039d56187ab0dbd8e776ec92ad8aaad47c50
Carmen Morata <cmoratagarcia@gmail.com>
2025-07-22 10:32:38 -0700

    Write tests for calculator

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverseString } = require("./index.js");
+const { capitalize, reverseString, calculator } = require("./index.js");
 //A capitalize function that takes a string and returns it with the first character capitalized.
 test("capitalizes first character", () => {
   expect(capitalize("abcdef")).toBe("Abcdef");
@@ -9,7 +9,18 @@ test("reverses string", () => {
   expect(reverseString("abCdef")).toBe("fedCba");
 });
 //A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
-
+test("adds 2 numbers", () => {
+  expect(calculator.add(10, 500)).toBe(600);
+});
+test("subtracts 2 numbers", () => {
+  expect(calculator.subtract(10, 500)).toBe(-400);
+});
+test("divides 2 numbers", () => {
+  expect(calculator.divide(10, 500)).toBe(0.02);
+});
+test("multiplies 2 numbers", () => {
+  expect(calculator.multiply(10, 500)).toBe(5000);
+});
 //A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”.
 
 //An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.
