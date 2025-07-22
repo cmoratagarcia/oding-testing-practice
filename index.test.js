ad61e3431c7c5bfb82e3c810ac8779a1fbd804d5
Carmen Morata <cmoratagarcia@gmail.com>
2025-07-22 10:21:42 -0700

    Write reverseString test

--- index.test.js
+++ index.test.js
@@ -1,11 +1,13 @@
-const { capitalize } = require("./index.js");
+const { capitalize, reverseString } = require("./index.js");
 //A capitalize function that takes a string and returns it with the first character capitalized.
 test("capitalizes first character", () => {
   expect(capitalize("abcdef")).toBe("Abcdef");
 });
 
 //A reverseString function that takes a string and returns it reversed.
-
+test("reverses string", () => {
+  expect(reverseString("abcdef")).toBe("fedcba");
+});
 //A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
 
 //A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”.
